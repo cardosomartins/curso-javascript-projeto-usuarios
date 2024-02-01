494260e0f37acdcab687e904092feba222b21e7f
Paulo Martins <paulohcmartins@hotmail.com>
2024-02-01 09:26:58 -0300

    updateVariables method created

--- dist/js/model/formModel.js
+++ dist/js/model/formModel.js
@@ -1,25 +1,37 @@
 class formModel{
     constructor(){
-       this._name = "hamster"
-       this._formEl = document.querySelectorAll("#form-user-create"); 
+       this._name;
+       this._gender;
+       this._birthday;
+       this._country;
+       this._email;
+       this._password;
+       this._photo;
+       this._adminBoolean;
+       this._formEl = document.getElementById("form-user-create"); 
        this.Initializer()
     }
 
+    updateVariables(){
+        this._name = this._formEl.querySelector("[id='exampleInputName']").value
+        // this._gender = this._formEl.querySelector("[id='exampleInput']").value
+        this._birthday = this._formEl.querySelector("[id='exampleInputBirth']").value
+        this._country = this._formEl.querySelector("[id='exampleInputCountry']").value
+        this._email = this._formEl.querySelector("[id='exampleInputEmail1']").value
+        this._password = this._formEl.querySelector("[id='exampleInputPassword1']").value
+        // this._photo = this._formEl.querySelector("[id='exampleInputFile']").value
+        this._adminBoolean = this._formEl.querySelector("[type='checkbox']").checked
+        console.log(this._adminBoolean)
+    }
 
     Initializer(){
 
         document.getElementById("form-user-create").addEventListener("submit", e => {
     
-            console.log("amebinha");
-            e.preventDefault();    
-            console.log(document.querySelectorAll("#exampleInputName"));
-            console.log(this._formEl)
-            // console.log(document.querySelector("#exampleInputName").getAttributeNames());
-         
-            // console.log(document.querySelector("#exampleInputName").getAttribute('name'));
-         
+            e.preventDefault();
+            this.updateVariables();
+            
           });
-
     }
 }
  
