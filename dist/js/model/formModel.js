48a3b2210538570f7d8dcaf2a7544275cab473e4
Paulo Martins <paulohcmartins@hotmail.com>
2024-02-01 22:10:29 -0300

    Functioning input and radio buttons

--- dist/js/model/formModel.js
+++ dist/js/model/formModel.js
@@ -13,15 +13,14 @@ class formModel{
     }
 
     updateVariables(){
-        this._name = this._formEl.querySelector("[id='exampleInputName']").value
-        // this._gender = this._formEl.querySelector("[id='exampleInput']").value
-        this._birthday = this._formEl.querySelector("[id='exampleInputBirth']").value
-        this._country = this._formEl.querySelector("[id='exampleInputCountry']").value
-        this._email = this._formEl.querySelector("[id='exampleInputEmail1']").value
-        this._password = this._formEl.querySelector("[id='exampleInputPassword1']").value
+        this._name = this._formEl.querySelector("[id='exampleInputName']").value;
+        this._gender = this._formEl.querySelector("[id='exampleInputGenderM']").checked ? "Masculino" : "Feminino";
+        this._birthday = this._formEl.querySelector("[id='exampleInputBirth']").value 
+        this._country = this._formEl.querySelector("[id='exampleInputCountry']").value;
+        this._email = this._formEl.querySelector("[id='exampleInputEmail1']").value;
+        this._password = this._formEl.querySelector("[id='exampleInputPassword1']").value;
         // this._photo = this._formEl.querySelector("[id='exampleInputFile']").value
-        this._adminBoolean = this._formEl.querySelector("[type='checkbox']").checked
-        console.log(this._adminBoolean)
+        this._adminBoolean = this._formEl.querySelector("[type='checkbox']").checked;
     }
 
     Initializer(){
