b855dac58f4b62e054fae03c3099df562db22930
Paulo Martins <paulohcmartins@hotmail.com>
2024-02-02 23:46:42 -0300

    Addition of row method created

--- dist/js/model/formModel.js
+++ dist/js/model/formModel.js
@@ -9,7 +9,29 @@ class formModel{
        this._photo;
        this._adminBoolean;
        this._formEl = document.getElementById("form-user-create"); 
+       this._mainPanel = document.querySelector(".table.table-striped");
+       this.addLine()
        this.Initializer()
+    }
+
+    addLine(){
+        console.log("Adicionando linhas");
+        let rowToBeAdded = document.createElement("tbody");
+        rowToBeAdded.innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${this._name}</td>
+            <td>${this._email}</td>
+            <td>${this._adminBoolean ? "Sim" : "Não"}</td>
+            <td>${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>`
+        this._mainPanel.append(rowToBeAdded);
+
+
     }
 
     updateVariables(){
@@ -19,7 +41,7 @@ class formModel{
         this._country = this._formEl.querySelector("[id='exampleInputCountry']").value;
         this._email = this._formEl.querySelector("[id='exampleInputEmail1']").value;
         this._password = this._formEl.querySelector("[id='exampleInputPassword1']").value;
-        // this._photo = this._formEl.querySelector("[id='exampleInputFile']").value
+        this._photo = this._formEl.querySelector("[id='exampleInputFile']").value
         this._adminBoolean = this._formEl.querySelector("[type='checkbox']").checked;
     }
 
@@ -29,7 +51,8 @@ class formModel{
     
             e.preventDefault();
             this.updateVariables();
-            
+            this.addLine();
+
           });
     }
 }
